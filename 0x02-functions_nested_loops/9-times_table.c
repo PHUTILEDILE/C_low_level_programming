57bc30a511b04347c4252578e24fe7d7a07a372d
phutiledile <phutiledile2@gmail.com>
2023-02-21 13:31:18 -0800

    print to 98

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,16 @@
 #include "main.h"
 
 /**
+ * times_table - prints the 9 times table
  *
+ * Return: always 0
+ */
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		_putchar("9 x %d = %d\n". i, 9 * i);
+	}
+}
